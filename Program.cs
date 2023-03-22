3a269463aaf5c607882cae2e0f6bb0901b6c07ac
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2023-03-22 16:41:32 -0400

    Main Gameplay Loop created

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using PlayerSys;
+using GameSystems;
 
 namespace HelloWorld
 {
@@ -9,33 +10,52 @@ namespace HelloWorld
             // Console.WriteLine("Hello, World!");
             Console.Clear();
 
-            Player pla = new Player();
+            Item goldBag = new Item("goldBag");
+            Item goldBag2 = new Item("goldBag");
+            Item goldBag3 = new Item("goldBag");
 
-            while (true)
-            {
-                Console.Write("What do you want to do: ");
-                string userInput = Console.ReadLine();
+            Console.WriteLine("Item Name: {0}", goldBag.itemName);
+            Console.WriteLine("Value: {0}", goldBag.value);
+            Console.WriteLine("Item Type: {0}", goldBag.itemType);
+            Console.WriteLine("Description: {0}", goldBag.description);
+            Console.WriteLine("----------------");
+            Console.WriteLine("Item Name: {0}", goldBag2.itemName);
+            Console.WriteLine("Value: {0}", goldBag2.value);
+            Console.WriteLine("Item Type: {0}", goldBag2.itemType);
+            Console.WriteLine("Description: {0}", goldBag2.description);
+            Console.WriteLine("----------------");
+            Console.WriteLine("Item Name: {0}", goldBag3.itemName);
+            Console.WriteLine("Value: {0}", goldBag3.value);
+            Console.WriteLine("Item Type: {0}", goldBag3.itemType);
+            Console.WriteLine("Description: {0}", goldBag3.description);
 
-                switch (userInput)
-                {
-                    case "gain":
-                        pla.expBar.gainExpericePoints();
-                        break;
-                    case "stats":
-                        Console.WriteLine(pla.name);
-                        // Health Bar Sys.
-                        Console.WriteLine(
-                            "Health: {0} / {1}",
-                            pla.healthBar.health,
-                            pla.healthBar.maxHealth
-                        );
-                        // Mana Bar Sys.
-                        Console.WriteLine("Mana: {0} / {1}", pla.manaBar.mana, pla.manaBar.maxMana);
-                        break;
-                        case "end":
-                            return;
-                }
-            }
+            // Player pla = new Player();
+
+            // while (true)
+            // {
+            //     Console.Write("What do you want to do: ");
+            //     string userInput = Console.ReadLine();
+
+            //     switch (userInput)
+            //     {
+            //         case "gain":
+            //             pla.expBar.gainExpericePoints();
+            //             break;
+            //         case "stats":
+            //             Console.WriteLine(pla.name);
+            //             // Health Bar Sys.
+            //             Console.WriteLine(
+            //                 "Health: {0} / {1}",
+            //                 pla.healthBar.health,
+            //                 pla.healthBar.maxHealth
+            //             );
+            //             // Mana Bar Sys.
+            //             Console.WriteLine("Mana: {0} / {1}", pla.manaBar.mana, pla.manaBar.maxMana);
+            //             break;
+            //             case "end":
+            //                 return;
+            //     }
+            // }
 
             // Add a constructor to the health & mana bar to generate for enemies default values
         }
