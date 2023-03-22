6f60c32064fa88b5b68721bb73193470831453fe
Jason Padilla <84742412+Jayleaf1125@users.noreply.github.com>
2023-03-21 20:36:33 -0400

    Quick gameplay loop

--- Program.cs
+++ Program.cs
@@ -11,23 +11,32 @@ namespace HelloWorld
 
             Player pla = new Player();
 
-            Console.WriteLine(pla.name);
-            // Health Bar Sys.
-            Console.WriteLine("Health: {0} / {1}", pla.healthBar.health, pla.healthBar.maxHealth);
-            // Mana Bar Sys.
-            Console.WriteLine("Mana: {0} / {1}", pla.manaBar.mana, pla.manaBar.maxMana);
-            // Experince Bar Sys.
-            pla.expBar.gainExpericePoints();
-            pla.expBar.gainExpericePoints();
-            pla.expBar.gainExpericePoints();
-            pla.expBar.gainExpericePoints();
-            // pla.expBar.gainExpericePoints();
-            Console.WriteLine("-----------");
-            Console.WriteLine(pla.name);
-            // Health Bar Sys.
-            Console.WriteLine("Health: {0} / {1}", pla.healthBar.health, pla.healthBar.maxHealth);
-            // Mana Bar Sys.
-            Console.WriteLine("Mana: {0} / {1}", pla.manaBar.mana, pla.manaBar.maxMana);
+            while (true)
+            {
+                Console.Write("What do you want to do: ");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "gain":
+                        pla.expBar.gainExpericePoints();
+                        break;
+                    case "check":
+                        Console.WriteLine(pla.name);
+                        // Health Bar Sys.
+                        Console.WriteLine(
+                            "Health: {0} / {1}",
+                            pla.healthBar.health,
+                            pla.healthBar.maxHealth
+                        );
+                        // Mana Bar Sys.
+                        Console.WriteLine("Mana: {0} / {1}", pla.manaBar.mana, pla.manaBar.maxMana);
+                        break;
+                        case "end":
+                            return;
+                            break;
+                }
+            }
 
             // Add a constructor to the health & mana bar to generate for enemies default values
         }
